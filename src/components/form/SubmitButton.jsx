cf89b608d561ec14eae513cc549c5e4819489a82
Igor <igor.paes88@gmail.com>
2024-05-29 21:23:43 -0300

    Implementado message e alguns outros detalhes de 'project'

--- src/components/form/SubmitButton.jsx
+++ src/components/form/SubmitButton.jsx
@@ -3,7 +3,7 @@ import styles from './SubmitButton.module.css'
 function SubmitButton({ text }) {
    return (
       <div>
-         <button type="button" className={styles.btn}>{text}</button>
+         <button type="submit" className={styles.btn}>{text}</button>
       </div>
    )
 }
--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,12 +1,37 @@
+import { useNavigate } from 'react-router-dom';
 import ProjectForm from '../project/ProjectForm';
-import styles from './NewProject.module.css'
+import styles from './NewProject.module.css';
 
 function NewProject() {
+
+   const navigate = useNavigate();
+
+   function createPost(project) {
+      
+      project.cost = 0;
+      project.services = [];
+
+      fetch("http://localhost:5000/projects", {
+         method: "POST",
+         headers: {
+            'Content-Type': 'application/json',
+         },
+         body: JSON.stringify(project)
+      }).then((resp) => resp.json())
+      .then((data) => {
+         navigate('/projects', {
+            replace: true,
+            state: { message: 'Projeto enviado com sucesso!' }
+         });   
+      }).catch(err => console.log(err));
+
+   }
+
    return (
       <div className={styles.newproject_container}>
          <h1>Criar Projeto</h1>
          <p>Crie o seu projeto para adicionar os serviços.</p>
-         <ProjectForm btnText="Criar projeto"/>
+         <ProjectForm handleSubmit={createPost} btnText="Criar projeto"/>
       </div>
    )
 }
